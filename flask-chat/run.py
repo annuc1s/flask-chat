093e841eb769c47dd8ca74245b5b3d1d42875fe8
annuc1s <annuciis121@gmail.com>
2020-05-06 17:00:36 +0000

    creating an index.html template

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
     """Add messages to the messages list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 def get_all_messages():
     """Get all of the messages and seperate them with a `br`"""
@@ -15,12 +17,12 @@ def get_all_messages():
 @app.route('/')
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}/.; {1}</h1>".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
