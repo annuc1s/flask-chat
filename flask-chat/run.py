f71e79d241afe88587cd3e68595ffe2cca39e057
annuc1s <annuciis121@gmail.com>
2020-05-06 19:21:24 +0000

    Refactoring To Use Chat.Html Instead Of A Single String

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -9,11 +9,9 @@ messages = []
 def add_messages(username, message):
     """Add messages to the messages list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
 
-def get_all_messages():
-    """Get all of the messages and seperate them with a `br`"""
-    return "<br>".join(messages)
+    messages.append(messages_dict)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,7 +28,7 @@ def index():
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
